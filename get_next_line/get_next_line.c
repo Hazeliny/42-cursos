e6473453a7405f13d4247f89186216fa1a728831
Lin Yao <linyao@cbr15s4.42barcelona.com>
2024-07-01 16:28:48 +0200

    get_next_line 1

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,11 +6,43 @@
 /*   By: linyao <linyao@student.42barcelona.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 13:09:34 by linyao            #+#    #+#             */
-/*   Updated: 2024/06/26 18:27:35 by linyao           ###   ########.fr       */
+/*   Updated: 2024/07/01 16:20:19 by linyao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static void	ft_free(char **storage)
+{
+	free(*storage);
+	*storage = NULL;
+}
+
+static char	*ft_extract_line(char **storage)
+{
+	size_t	i;
+	char	*line;
+	char	*temp;
+
+	i = 0;
+	while ((*storage)[i] && (*storage)[i] != '\n')
+		i++;
+	if ((*storage)[i] == '\n')
+	{
+		line = ft_strndup(*storage, i + 1);
+		temp = ft_strdup(*storage + i + 1);
+		free(*storage);
+		*storage = temp;
+		if (**storage == '\0')
+			ft_free(storage);
+	}
+	else
+	{
+		line = ft_strdup(*storage);
+		ft_free(storage);
+	}
+	return (line);
+}
 
 static int	ft_read_file(int fd, char **storage)
 {
@@ -39,10 +71,11 @@ char	*get_next_line(int fd)
 	char		*line;
 	ssize_t		bytes_read;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	storage = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
 	bytes_read = 1;
-	while (!ft_strchr(storage, '\n') && bytes_read > 0)
+	while (storage == NULL || !ft_strchr(storage, '\n') && bytes_read > 0)
 		bytes_read = ft_read_file(fd, &storage);
 	if (bytes_read < 0)
 		return (NULL);
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: linyao <linyao@student.42barcelona.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 13:04:44 by linyao            #+#    #+#             */
-/*   Updated: 2024/07/01 15:27:29 by linyao           ###   ########.fr       */
+/*   Updated: 2024/07/01 16:27:03 by linyao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,14 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-char	*get_next_line(int fd);
-size_t	ft_strlen(char *str);
-size_t	ft_strlcpy(char *dst, char *src, size_t dstsize);
-char	*ft_strchr(char *str, char c);
-char	*ft_strdup(char *src);
-char	*ft_strjoin(char *s1, char *s2);
+char		*get_next_line(int fd);
+static int	ft_read_file(int fd, char **storage);
+static char	*ft_extract_line(char **storage);
+static void	ft_free(char **storage);
+size_t		ft_strlen(char *str);
+char		*ft_strchr(char *str, char c);
+char		*ft_strjoin(char *s1, char *s2);
+char		*ft_strdup(char *s);
+char		*ft_strndup(char *s, size_t n);
 
 #endif
